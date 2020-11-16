1429a84bc6ca8ed18321c8284ada6190afc1ecaf
Sangseok Doe <sangseoksoftware@outlook.com>
2020-11-16 17:04:40 -0500

    #8 Lecture Done

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -134,3 +133,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 AUTH_USER_MODEL = "users.User"
+
+MEDIA_ROOT = BASE_DIR / "uploads"
+
+MEDIA_URL = "/media/"
